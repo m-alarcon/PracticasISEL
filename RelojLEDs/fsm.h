a025f9e2c783f7092db116843383d0c1cec08296
m-alarcon <m.alarcongra@gmail.com>
2015-05-05 16:28:26 +0200

    Update fsm.h

--- RelojLEDs/fsm.h
+++ RelojLEDs/fsm.h
@@ -4,27 +4,25 @@
 #define MAXSTATES 10
 #define MAXINS 32
 
-struct fsm_t;
+typedef struct fsm_t fsm_t;
 
-typedef void (*fsm_output_func_t) (struct fsm_t*);
+typedef int (*fsm_input_func_t) (fsm_t*);
+typedef void (*fsm_output_func_t) (fsm_t*);
 
 typedef struct fsm_trans_t {
   int orig_state;
-  int in;
+  fsm_input_func_t in;
   int dest_state;
-  fsm_output_func_t run;
+  fsm_output_func_t out;
 } fsm_trans_t;
 
-typedef struct fsm_t {
+struct fsm_t {
   int current_state;
   fsm_trans_t* tt;
-  /* optimized implementation */
-  int next_state[MAXSTATES][MAXINS];
-  fsm_output_func_t run[MAXSTATES][MAXINS];
-} fsm_t;
+};
 
 fsm_t* fsm_new (fsm_trans_t* tt);
 void fsm_init (fsm_t* this, fsm_trans_t* tt);
-void fsm_fire (fsm_t* this, int in);
+void fsm_fire (fsm_t* this);
 
 #endif
